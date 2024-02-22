d8d0e06f837bc158f4f67783cc734b5fb6fa4037
shirtsandforks <shirtsandforks@gmail.com>
2024-02-23 01:15:26 +0200

    Added navbar component

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,11 @@
+import Link from "next/link";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link href="/properties">Show properties</Link>
+    </div>
+  );
 };
 export default Homepage;
